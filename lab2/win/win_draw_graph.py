130a5425dcc45b31a97725d7103378c33aeefeff
tim <timaryadovou@yandex.ru>
2023-10-11 16:38:29 +0300

    update 11/10/2023

--- lab2/win/win_draw_graph.py
+++ lab2/win/win_draw_graph.py
@@ -2,12 +2,12 @@ import csv
 import matplotlib.pyplot as plt
 
 def graph():
-    with open("linux_make_csv.py", "r", encoding="utf-8") as file:
+    with open("win_make_csv.py", "r", encoding="utf-8") as file:
         data = list(csv.reader(file))[1:]
         free_memory = [int(line[0]) for line in data]
         date_time = [_+1 for _ in range(len(data))]
     plt.xlabel('date_time')
-    plt.ylabel('free_memorry')
+    plt.ylabel('free_memory')
     plt.plot(date_time, free_memory, 'o-g', label='prcs(t)')
     plt.legend()
     plt.show()
